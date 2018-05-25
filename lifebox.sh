05d94478d39ef0e13cfa758d47b55aa31d153805
lycros <39453184+lycros@users.noreply.github.com>
2018-05-24 21:27:04 -0700

    Update lifebox.sh

--- lifebox.sh
+++ lifebox.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+echo "Creating lifebox directory"
+mkdir rpi-rgb-led-matrix/lifebox/
+echo "Download lifebox.cc"
+wget https://github.com/lycros/lifebox/raw/master/lifebox.cc
+echo "Download Makefile
+wget https://github.com/lycros/lifebox/raw/master/Makefile
+echo "Moving Files"
+mv lifebox.cc rpi-rgb-led-matrix/lifebox/
+mv Makefile rpi-rgb-led-matrix/lifebox/
+make
+echo "End Script"
